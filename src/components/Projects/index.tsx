5fcbd7cbc9630c612d25ce65b5693f6b50947b2c
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2024-03-02 13:41:44 -0300

    refactor: wrap with Container

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,4 +1,5 @@
 import useRepos from "../../utils/githubApi";
+import Container from "../Container";
 import ProjectCard from "./ProjectCard";
 import "./Projects.scss";
 
@@ -10,30 +11,32 @@ export default function Projects({ sectionId }: ProjectsProps) {
   const repos = useRepos();
 
   return (
-    <section className="projects-container" id={sectionId}>
-      <h1 className="projects-section-title">Projects</h1>
-      {repos.map((repo, index) => {
-        const title = repo.name
-          .split("-")
-          .map((word) => {
-            return word[0].toUpperCase() + word.substring(1);
-          })
-          .join(" ");
+    <Container>
+      <section className="projects-container" id={sectionId}>
+        <h1 className="projects-section-title">Projects</h1>
+        {repos.map((repo, index) => {
+          const title = repo.name
+            .split("-")
+            .map((word) => {
+              return word[0].toUpperCase() + word.substring(1);
+            })
+            .join(" ");
 
-        if (repo.name != "AdmFin")
-          return (
-            <ProjectCard
-              className={`${index % 2 === 0 && "reverse"}`}
-              id={repo.id}
-              image={`/src/assets/images/${repo.name}.webp`}
-              imageAlt={repo.name}
-              linkToGithub={repo.html_url}
-              title={title}
-              description={repo.description}
-              liveProject={`https://lmiguelcolombo.github.io/${repo.name}`}
-            />
-          );
-      })}
-    </section>
+          if (repo.name != "AdmFin")
+            return (
+              <ProjectCard
+                className={`${index % 2 === 0 && "reverse"}`}
+                id={repo.id}
+                image={`/src/assets/images/${repo.name}.webp`}
+                imageAlt={repo.name}
+                linkToGithub={repo.html_url}
+                title={title}
+                description={repo.description}
+                liveProject={`https://lmiguelcolombo.github.io/${repo.name}`}
+              />
+            );
+        })}
+      </section>
+    </Container>
   );
 }
